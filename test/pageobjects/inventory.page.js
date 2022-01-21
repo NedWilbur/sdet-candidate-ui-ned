a7b60c4dbd7fa9b90bf8e58a83626ab08920bccf
Ned Wilbur <Ned612@Gmail.com>
2022-01-21 09:48:02 -0500

    Added inventory.page 'addItemToCart' and 'clickItemTitle'

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -10,6 +10,28 @@ class InventoryPage extends Page {
     get secondaryTitle() {
         return $('#header_container > div.header_secondary_container > span')
     }
+
+    // TODO: Add locators & methods *ByName and *ById
+    itemContainerByIndex(index = 1) {
+        return $(`//div[@class='inventory_item'][${index}]`);
+    }
+
+    itemAddButtonByIndex(index = 1) {
+        return this.itemContainerByIndex(index).$("//button[.='Add to cart']");
+    }
+
+    itemTitleButtonByIndex(index = 1) {
+        return this.itemContainerByIndex(index).$('.inventory_item_name');
+    }
+
+    /* Methods */
+    addItemToCart(index = 1) { 
+        return this.itemAddButtonByIndex(index).click();
+    }
+
+    clickItemTitle(index = 1) {
+        return this.itemTitleButtonByIndex(index).click();
+    } 
 }
 
 module.exports = new InventoryPage();
