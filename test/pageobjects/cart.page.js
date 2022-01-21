9731fa7796fec9c715095786ece149353f5efea0
Ned Wilbur <Ned612@Gmail.com>
2022-01-21 11:08:44 -0500

    Added cartItemExist(item) validation method

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,12 +1,25 @@
 class CartPage  {
     /* Locators */
+    get CartItems() {
+        return $$('.cart_item');
+    }
+
     get CheckoutButton() {
         return $('#checkout');
     }
 
     /* Methods */        
-    clickCheckoutButton() {
-        return this.CheckoutButton.click();
+    async clickCheckoutButton() {
+        await this.CheckoutButton.click();
+    }
+
+    async cartItemExist(item) {
+        let allItems = await this.CartItems;
+        for (const itemElement of allItems){
+            let title = await itemElement.$('.inventory_item_name').getText()
+            if (title == item.title) return true;
+        }
+        return false;
     }
 }
 
