015c65ba6338efc708e67831ce8ee4f0755937c2
Ned Wilbur <Ned612@Gmail.com>
2022-01-21 11:28:41 -0500

    Added TC 'should purchase single item added from inventory page'

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,11 +1,9 @@
 const LoginPage = require('../pageobjects/login.page');
 const InventoryPage = require('../pageobjects/inventory.page');
-const HeaderPage = require('../pageobjects/header.page');
-const CartPage = require('../pageobjects/cart.page');
-const CheckoutInfoPage = require('../pageobjects/checkout/checkoutinfo.page');
-const CheckoutOverviewPage = require('../pageobjects/checkout/checkoutoverview.page');
-const CheckoutCompletePage = require('../pageobjects/checkout/checkoutcomplete.page');
+const Workflows = require('../workflows');
+const User = require('../objects/user');
 
+const user = new User("Swag", "User", "12345");
 
 describe('Sauce Demo', () => {
     beforeEach(async () => {
@@ -13,14 +11,47 @@ describe('Sauce Demo', () => {
         await LoginPage.login('standard_user', 'secret_sauce');
     });
 
-    it('Sandbox', async () => {
+    it.skip('Sandbox', async () => {
         let itemIndex = 1;
-        await InventoryPage.addItemToCart(itemIndex);
+        let items  = []
+
+        await InventoryPage.addItemToCart(1);
+        await InventoryPage.addItemToCart(2);
+        await InventoryPage.addItemToCart(3);
+
+        items.push(await InventoryPage.getItemDetails(1));
+        items.push(await InventoryPage.getItemDetails(2));
+        items.push(await InventoryPage.getItemDetails(3));
+
+
         await InventoryPage.clickItemTitle(itemIndex);
-        await HeaderPage.clickOnCart();
-        await CartPage.clickCheckoutButton();
-        await CheckoutInfoPage.inputInfo("Swag", "Labs", "12345", true)
-        await CheckoutOverviewPage.clickFinishButton();
-        await expect(CheckoutCompletePage.completeHeader).toHaveTextContaining(CheckoutCompletePage.SuccessfulOrderHeader);
+        // await HeaderPage.clickOnCart();
+        // await expect(await CartPage.cartItemExist(item)).toBe(true);
+        // await CartPage.clickCheckoutButton();
+        // await CheckoutInfoPage.inputInfo("Swag", "Labs", "12345", true)
+        // await CheckoutOverviewPage.clickFinishButton();
+        // await expect(CheckoutCompletePage.completeHeader).toHaveTextContaining(CheckoutCompletePage.SuccessfulOrderHeader);
+        await Workflows.Checkout(user, items);
+    });
+
+    it('should purchase single item added from inventory page', async () => {
+        let item = await InventoryPage.addItemToCart();
+        await Workflows.Checkout(user, item);
+    });
+
+    it.skip('should purchase single item added from item page', async () => {
+
+    });
+
+    it.skip('should purchase multiple items added from inventory page', async () => {
+    
+    });
+
+    it.skip('should purchase multiple items added from inventory page', async () => {
+    
+    });
+
+    it.skip('should purchase multiple items added from inventory and item page', async () => {
+    
     });
 });
